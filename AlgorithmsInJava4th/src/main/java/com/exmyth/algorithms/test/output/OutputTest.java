03aef8bbfb6e98f24dd0d443f464d06264afd76a
钟马明 <maming.zhong@tongdun.cn>
2016-12-06 23:20:20 +0800

    add java output demo

---
+++ AlgorithmsInJava4th/src/main/java/com/exmyth/algorithms/test/output/OutputTest.java
@@ -0,0 +1,12 @@
+package com.exmyth.algorithms.test.output;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by exmyth on 16/12/6.
+ */
+public class OutputTest {
+    public static void main(String[] args) {
+        StdOut.println("HelloWorld!");
+    }
+}
