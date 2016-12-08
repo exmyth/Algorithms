6198fd4193cbf0833bff4c09ae8fecf31f9221c4
钟马明 <maming.zhong@tongdun.cn>
2016-12-08 15:13:39 +0800

    ignore empty string

--- AlgorithmsInJava4th/src/test/java/com/exmyth/algorithms/test/regex/RegexTest.java
+++ AlgorithmsInJava4th/src/test/java/com/exmyth/algorithms/test/regex/RegexTest.java
@@ -8,13 +8,47 @@ import java.util.regex.Pattern;
  */
 public class RegexTest {
     public static void main(String[] args) {
-        String documentTxt = "abc   \n$(\"#cusName\").html(\"杨xx\");\n"
+        String documentTxt = "if (!comm) {\n"
+                        + "\t\t\t$(\"#validCodeTip\").html(\n"
+                        + "\t\t\t\t\t\"<IMG src=\\\"http://qh.189.cn:10001/r/cms/ah/default/images/onError.gif\\\"/><font color=\\\"red\\\">\"\n"
+                        + "\t\t\t\t\t\t\t+ \"请输入正确的验证码!\" + \"</font>\");\n"
+                        + "\t\t\treturn;\n"
+                        + "\t\t}\n"
+                        + "\t\t$(\"#cusName\").html(\"\");\n"
+                        + "\t\t$(\"#latnName\").html(\"\");\n"
+                        + "\t\t$(\"#address\").html(\"\");\n"
+                        + "\t\t$(\"#cardNum\").html(\"\");\n"
+                        + "\t\t$(\"#custId\").html(\"\");\n"
+                        + "\t\tif (isInvSms) {\n"
+                        + "\t\t\tvar validationSms = $(\"#validationSmsInput\").val();\n"
+                        + "\t\t\tif (!validationSms) {\n"
+                        + "\t\t\t\talert(\"短信已经发出,请输入短信验证码！\");\n"
+                        + "\t\t\t\treturn;\n"
+                        + "\t\t\t}\n"
+                        + "\t\t\t$.ajax({\n"
+                        + "\t\t\t\ttype : \"post\",\n"
+                        + "\t\t\t\turl : \"/service/passmanage/smsRandomCustInfo.action\",\n"
+                        + "\t\t\t\tdata : {\n"
+                        + "\t\t\t\t\t\"validationSms\" : validationSms\n"
+                        + "\t\t\t\t},\n"
+                        + "\t\t\t\tdataType : \"json\",\n"
+                        + "\t\t\t\tsuccess : function(data) {\n"
+                        + "\t\t\t\t\tif (data != null && data.flag == '1') {\n"
+                        + "\t\t\t\t\t\t//prodStatus(\"aaa\");\n"
+                        + "\t\t\t\t\t\t$(\"#cusName\").html(\"杨小兰\");\n"
                         + "\t\t\t\t\t\t$(\"#latnName\").html(\"西宁市\");\n"
                         + "\t\t\t\t\t\t$(\"#address\").html(\"西宁市城北区柴达木路214号4栋361室\");\n"
-                        + "\t\t\t\t\t\t$(\"#cardNum\").html(\"51292319xxxx116489\");\n"
-                        + "\t\t\t\t\t\t$(\"#custId\").html(\"713008xxxx88\");\n   $(";
+                        + "\t\t\t\t\t\t$(\"#cardNum\").html(\"512923196209116489\");\n"
+                        + "\t\t\t\t\t\t$(\"#custId\").html(\"713008116688\");\n"
+                        + "\t\t\t\t\t\t$(\"#custStatus\").html(prodStatus(\"1\"));\n"
+                        + "\t\t\t\t\t} else {\n"
+                        + "\t\t\t\t\t\talert(\"随机码发送失败:\" + data.msg);\n"
+                        + "\t\t\t\t\t}\n"
+                        + "\t\t\t\t}\n"
+                        + "\t\t\t});\n"
+                        + "\t\t} else ";
         String str=documentTxt.replaceAll("[\\t\\n\\r]", "");//将内容区域的回车换行去除
-        Pattern p = Pattern.compile(".*?\\$\\(\"#cusName\"\\).html\\(\"(.*?)\"\\);"+
+        Pattern p = Pattern.compile(".*?;\\$\\(\"#cusName\"\\).html\\(\"(.*?)\"\\);"+
                         "\\$\\(\"#latnName\"\\).html\\(\"(.*?)\"\\);"+
                         "\\$\\(\"#address\"\\).html\\(\"(.*?)\"\\);"+
                         "\\$\\(\"#cardNum\"\\)\\.html\\(\"(.*?)\"\\);"+
